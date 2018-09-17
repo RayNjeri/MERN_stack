37fe909df47188ca4a6c5654eff06fa7d304e5c1
Rachael Njeri <rachael.njeri@andela.com>
2018-09-17 21:45:43 +0300

    database connection
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db)
+  .then(() => console.log('DB connected'))
+  .catch(err => console.log('err', err));
 
 app.get('/', (req, res) => res.send('Hello world'));
 
