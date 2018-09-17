4488a1f5bcefab6097d371f36b90910e044b918d
Rachael Njeri <rachael.njeri@andela.com>
2018-09-17 22:33:56 +0300

    Users model, registration and hashing
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+const User = require('../../models/Users');
 
 router.get('/test', (req, res) =>
   res.json({
     message: 'users works'
   })
 );
+
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: 'Email already exists' });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: '200',
+        r: 'pg',
+        default: 'mm'
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log('error'));
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
